d9279e52ceeba2d8dc5c37006e7636229f938561
carltonprananda <37216812+carltonprananda@users.noreply.github.com>
2020-05-08 08:48:08 +0800

    Progress Pertama
    Tampilan Login Register sudah selesai, Tampilan Profile belum dan loginregister activity baru sedikit.

--- app/src/main/java/com/example/app/ai_uas/LoginRegsiterActivity.java
+++ app/src/main/java/com/example/app/ai_uas/LoginRegsiterActivity.java
@@ -3,12 +3,57 @@ package com.example.app.ai_uas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginRegsiterActivity extends AppCompatActivity {
 
+    Button loginbutton, registerbutton;
+    //login
+    EditText usernamelogin, passwordlogin;
+    //register
+    EditText firstnamereg, middlenamereg, lastnamereg, facultyreg, yearreg, usernamereg, passwordreg;
+    FirebaseAuth firebaseAuth;
+    ProgressBar progressBarlogin, progressBarregister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_regsiter);
+
+        loginbutton = findViewById(R.id.loginbutton);
+        registerbutton = findViewById(R.id.registerbutton);
+        //login
+        usernamelogin = findViewById(R.id.loginusername);
+        passwordlogin = findViewById(R.id.loginpassword);
+        //register
+        firstnamereg = findViewById(R.id.regfirstname);
+        middlenamereg = findViewById(R.id.regmidname);
+        lastnamereg = findViewById(R.id.reglastname);
+        facultyreg = findViewById(R.id.regfik);
+        yearreg = findViewById(R.id.regtahun);
+        usernamereg = findViewById(R.id.regusername);
+        passwordreg = findViewById(R.id.regpassword);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        progressBarlogin = findViewById(R.id.loginprogressBar);
+        progressBarregister = findViewById(R.id.registerprogressBar);
+
+        loginbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        registerbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String firstname = firstnamereg.getText().toString().trim();
+                
+            }
+        });
     }
 }
