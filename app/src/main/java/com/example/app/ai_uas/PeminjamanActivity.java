b591174598d52ba9470ace43b06855235d14b8ae
jeems - UC <jagustinus@student.ciputra.ac.id>
2020-05-11 09:39:26 +0700

    facerecog

---
+++ app/src/main/java/com/example/app/ai_uas/PeminjamanActivity.java
@@ -0,0 +1,140 @@
+package com.example.app.ai_uas;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Toast;
+
+import com.soundcloud.android.crop.Crop;
+
+import java.io.File;
+import java.io.IOException;
+
+public class PeminjamanActivity extends AppCompatActivity {
+
+    // for permission requests
+    public static final int REQUEST_PERMISSION = 300;
+
+    // request code for permission requests to the os for image
+    public static final int REQUEST_IMAGE = 100;
+
+    // will hold uri of image obtained from camera
+    private Uri imageUri;
+
+    // string to send to next activity that describes the chosen classifier
+    private String chosen;
+
+    //boolean value dictating if chosen model is quantized version or not.
+    private boolean quant;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_peminjaman);
+
+        // request permission to use the camera on the user's phone
+//        if (ActivityCompat.checkSelfPermission(this.getApplicationContext(), android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+//            ActivityCompat.requestPermissions(this, new String[] {android.Manifest.permission.CAMERA}, REQUEST_PERMISSION);
+//        }
+
+        // request permission to write data (aka images) to the user's external storage of their phone
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_PERMISSION);
+        }
+
+        // request permission to read data (aka images) from the user's external storage of their phone
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    REQUEST_PERMISSION);
+        }
+
+
+
+        chosen = "model_unquant.tflite";
+        // model in not quantized
+        quant = false;
+
+
+        findViewById(R.id.peminjaman_btn_take_image).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCameraIntent();
+            }
+        });
+    }
+
+
+    private void openCameraIntent(){
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, "New Picture");
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From your Camera");
+        // tell camera where to store the resulting picture
+        imageUri = getContentResolver().insert(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
+        // start camera, and wait for it to finish
+        startActivityForResult(intent, REQUEST_IMAGE);
+    }
+
+    // checks that the user has allowed all the required permission of read and write and camera. If not, notify the user and close the application
+    @Override
+    public void onRequestPermissionsResult(final int requestCode, @NonNull final String[] permissions, @NonNull final int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_PERMISSION) {
+            if (!(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                Toast.makeText(getApplicationContext(),"This application needs read, write, and camera permissions to run. Application now closing.", Toast.LENGTH_LONG);
+                System.exit(0);
+            }
+        }
+    }
+
+    // dictates what to do after the user takes an image, selects and image, or crops an image
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        // if the camera activity is finished, obtained the uri, crop it to make it square, and send it to 'Classify' activity
+        if(requestCode == REQUEST_IMAGE && resultCode == RESULT_OK) {
+            try {
+                Uri source_uri = imageUri;
+                Uri dest_uri = Uri.fromFile(new File(getCacheDir(), "cropped"));
+                // need to crop it to square image as CNN's always required square input
+                Crop.of(source_uri, dest_uri).asSquare().start(PeminjamanActivity.this);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        // if cropping acitivty is finished, get the resulting cropped image uri and send it to 'Classify' activity
+        else if(requestCode == Crop.REQUEST_CROP && resultCode == RESULT_OK){
+            imageUri = Crop.getOutput(data);
+            Intent i = new Intent(PeminjamanActivity.this, DetectFaceActivity.class);
+            Toast.makeText(this,imageUri.toString(),Toast.LENGTH_LONG).show();
+            // put image data in extras to send
+            i.putExtra("resID_uri", imageUri);
+            // put filename in extras
+            i.putExtra("chosen", chosen);
+            // put model type in extras
+            i.putExtra("quant", quant);
+            // send other required data
+            startActivity(i);
+        }
+    }
+
+}
--- app/src/main/java/com/example/app/ai_uas/fragment/HistoryFragment.java
+++ app/src/main/java/com/example/app/ai_uas/fragment/HistoryFragment.java
@@ -1,19 +1,43 @@
 package com.example.app.ai_uas.fragment;
 
+import android.Manifest;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.app.ai_uas.PeminjamanActivity;
 import com.example.app.ai_uas.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HistoryFragment extends Fragment {
+
+    // for permission requests
+    public static final int REQUEST_PERMISSION = 300;
+
+    // will hold uri of image obtained from camera
+    private Uri imageUri;
+
+    // string to send to next activity that describes the chosen classifier
+    private String chosen;
+
+    //boolean value dictating if chosen model is quantized version or not.
+    private boolean quant;
+
 
     public HistoryFragment() {
         // Required empty public constructor
@@ -26,13 +50,34 @@ public class HistoryFragment extends Fragment {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_history, container, false);
 
+
+
+
+
+
         view.findViewById(R.id.fab_peminjaman).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(getActivity(), PeminjamanActivity.class);
+                startActivity(intent);
             }
         });
 
         return view;
     }
+
+    // opens camera for user
+//    private void openCameraIntent(){
+//        ContentValues values = new ContentValues();
+//        values.put(MediaStore.Images.Media.TITLE, "New Picture");
+//        values.put(MediaStore.Images.Media.DESCRIPTION, "From your Camera");
+//        // tell camera where to store the resulting picture
+//        imageUri = getContentResolver().insert(
+//                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+//        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+//        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+//        // start camera, and wait for it to finish
+//        startActivityForResult(intent, REQUEST_IMAGE);
+//    }
 }
